678971679669930940bee4169a1169a91f66ffba
strannik-vka <canbka13@gmail.com>
2020-12-03 16:53:19 +0300

    fix

--- zoom.js
+++ zoom.js
@@ -1,10 +1,10 @@
 // Масштабирование
 function zoom() {
     if ($(window).width() > 1200 && $(window).width() < 1900) {
-        var original = 1900, percent = ((original - $(window).width()) / ((original + $(window).width()) / 2)) * 100;
-        $('html').css('zoom', (100 - (percent - 5)) + '%');
+        var original = 1900, percent = 100 * (original - $(window).width()) / $(window).width();
+        $('html').css('zoom', (100 - percent) + '%');
     } else if ($(window).width() > 1925) {
-        var original = 1900, percent = (($(window).width() - original) / ((original + $(window).width()) / 2)) * 100;
+        var original = 1900, percent = 100 * ($(window).width() - original) / original;
         $('html').css('zoom', (100 + percent) + '%');
     } else {
         $('html').css('zoom', 'unset');
