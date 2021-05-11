ca93421bceeb1e4f6fdf03ca3e5b99b84c83eb49
serj <zenitovic@gmail.com>
2021-05-11 19:11:21 +0300

    Update label_up.js

--- label_up.js
+++ label_up.js
@@ -1,38 +1,17 @@
-$('input.form-control').each(function() {
+$('input.form-control', 'input.form-type-text', 'textarea.form-type-text').each(function() {
     if ($(this).prev().length) {
         if ($(this).prev()[0].tagName == 'LABEL' && $.trim($(this).val())) {
             $(this).prev().addClass('label-up');
         }
     }
 });
-$(document).on('focus', 'input.form-control', function() {
+$(document).on('focus', 'input.form-control, input.form-type-text, textarea.form-type-text', function() {
     if ($(this).prev().length) {
         if ($(this).prev()[0].tagName == 'LABEL') {
             $(this).prev().addClass('label-up');
         }
     }
-}).on('blur', 'input.form-control', function() {
-    if ($(this).prev().length) {
-        if ($(this).prev()[0].tagName == 'LABEL' && !$.trim($(this).val())) {
-            $(this).prev().removeClass('label-up');
-        }
-    }
-});
-
-$('input.form-type-text').each(function() {
-    if ($(this).prev().length) {
-        if ($(this).prev()[0].tagName == 'LABEL' && $.trim($(this).val())) {
-            $(this).prev().addClass('label-up');
-        }
-    }
-});
-$(document).on('focus', 'input.form-type-text', function() {
-    if ($(this).prev().length) {
-        if ($(this).prev()[0].tagName == 'LABEL') {
-            $(this).prev().addClass('label-up');
-        }
-    }
-}).on('blur', 'input.form-type-text', function() {
+}).on('blur', 'input.form-control, input.form-type-text, textarea.form-type-text', function() {
     if ($(this).prev().length) {
         if ($(this).prev()[0].tagName == 'LABEL' && !$.trim($(this).val())) {
             $(this).prev().removeClass('label-up');
