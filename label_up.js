bf22f2567e54291af67f4374d05eb9529d329b09
serj <zenitovic@gmail.com>
2021-05-11 18:50:27 +0300

    Update label_up.js

--- label_up.js
+++ label_up.js
@@ -1,20 +1,41 @@
-$('input.form-control').each(function () {
-	if($(this).prev().length){
-		if ($(this).prev()[0].tagName == 'LABEL' && $.trim($(this).val())) {
-			$(this).prev().addClass('label-up');
-		}
-	}
+$('input.form-control').each(function() {
+    if ($(this).prev().length) {
+        if ($(this).prev()[0].tagName == 'LABEL' && $.trim($(this).val())) {
+            $(this).prev().addClass('label-up');
+        }
+    }
 });
-$(document).on('focus', 'input.form-control', function () {
-	if($(this).prev().length){
-		if ($(this).prev()[0].tagName == 'LABEL') {
-			$(this).prev().addClass('label-up');
-		}
-	}
-}).on('blur', 'input.form-control', function () {
-	if($(this).prev().length){
-		if ($(this).prev()[0].tagName == 'LABEL' && !$.trim($(this).val())) {
-			$(this).prev().removeClass('label-up');
-		}
-	}
+$(document).on('focus', 'input.form-control', function() {
+    if ($(this).prev().length) {
+        if ($(this).prev()[0].tagName == 'LABEL') {
+            $(this).prev().addClass('label-up');
+        }
+    }
+}).on('blur', 'input.form-control', function() {
+    if ($(this).prev().length) {
+        if ($(this).prev()[0].tagName == 'LABEL' && !$.trim($(this).val())) {
+            $(this).prev().removeClass('label-up');
+        }
+    }
+});
+
+$('input.form-type-text').each(function() {
+    if ($(this).prev().length) {
+        if ($(this).prev()[0].tagName == 'LABEL' && $.trim($(this).val())) {
+            $(this).prev().addClass('label-up');
+        }
+    }
+});
+$(document).on('focus', 'input.form-type-text', function() {
+    if ($(this).prev().length) {
+        if ($(this).prev()[0].tagName == 'LABEL') {
+            $(this).prev().addClass('label-up');
+        }
+    }
+}).on('blur', 'input.form-type-text', function() {
+    if ($(this).prev().length) {
+        if ($(this).prev()[0].tagName == 'LABEL' && !$.trim($(this).val())) {
+            $(this).prev().removeClass('label-up');
+        }
+    }
 });
