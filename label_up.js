859301e77c27faffc35b2ca0f23ae4ef29d1a2eb
strannik-vka <canbka13@gmail.com>
2021-05-11 16:54:13 +0300

    sad

--- label_up.js
+++ label_up.js
@@ -1,20 +1,20 @@
 $('input.form-control').each(function () {
 	if($(this).prev().length){
 		if ($(this).prev()[0].tagName == 'LABEL' && $.trim($(this).val())) {
-			$(this).prev().addClass('label-group');
+			$(this).prev().addClass('label-up');
 		}
 	}
 });
 $(document).on('focus', 'input.form-control', function () {
 	if($(this).prev().length){
 		if ($(this).prev()[0].tagName == 'LABEL') {
-			$(this).prev().addClass('label-group');
+			$(this).prev().addClass('label-up');
 		}
 	}
 }).on('blur', 'input.form-control', function () {
 	if($(this).prev().length){
 		if ($(this).prev()[0].tagName == 'LABEL' && !$.trim($(this).val())) {
-			$(this).prev().removeClass('label-group');
+			$(this).prev().removeClass('label-up');
 		}
 	}
 });
