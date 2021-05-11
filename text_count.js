727627e560f19171ea70edb39ef5918fce0b9033
serj <zenitovic@gmail.com>
2021-05-12 01:21:46 +0300

    Update text_count.js

--- text_count.js
+++ text_count.js
@@ -1,12 +1,12 @@
 var text_count = {
 
-    init: function () {
+    init: function() {
         $(document).on('input', '[data-input-count]', text_count.change);
         text_count.fill();
     },
 
-    fill: function () {
-        $('[data-input-count]').each(function () {
+    fill: function() {
+        $('[data-input-count]').each(function() {
             text_count.set(
                 $('[data-output-count="' + $(this).attr('name') + '"]'),
                 text_count.count($(this))
@@ -14,18 +14,26 @@ var text_count = {
         });
     },
 
-    change: function () {
+    change: function() {
         text_count.set(
             $('[data-output-count="' + $(this).attr('name') + '"]'),
             text_count.count($(this))
         );
     },
 
-    set: function (elem, count) {
-        elem.text('Введено ' + count);
+    set: function(elem, count) {
+        elem.text(count);
+
+        var max = parseFloat(elem.attr('data-max-count'));
+
+        if (count > max) {
+            elem.addClass('higher');
+        } else {
+            elem.removeClass('higher');
+        }
     },
 
-    count: function (elem) {
+    count: function(elem) {
         return $.trim(elem.val()).replace(/\n/g, '\n\r').length;
     }
 
