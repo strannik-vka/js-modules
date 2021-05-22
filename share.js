c9e68ca6715f3c0ee42b03b4593f260e2201fdaa
strannik-vka <canbka13@gmail.com>
2021-05-22 19:50:51 +0300

    fix

--- share.js
+++ share.js
@@ -7,27 +7,25 @@ var share = {
         vk: 'http://vk.com/share.php?url=URL&title=TITLE'
     },
 
-    social: function(name, url, title){
+    social: function (name, url, title) {
         url = url ? url : window.location.href;
         title = title ? title : $('title').text();
         return share.data[name].replace('URL', encodeURIComponent(url)).replace('TITLE', title);
     },
 
-    init: function(){
-        $(document).on('click', '[share-link]', function(){
+    init: function () {
+        $(document).on('click', '[share-link]', function () {
             var name = $(this).attr('share-link'),
                 url = $(this).attr('share-url'),
                 title = $(this).attr('share-title'),
                 href = share.social(name, url, title);
 
             $('#share_link').remove();
-            $('body').append('<a target="_blank" id="share_link" href="'+ href +'" class="d-none"></a>');
+            $('body').append('<a target="_blank" id="share_link" href="' + href + '" class="d-none"></a>');
             $('#share_link')[0].click();
         });
     }
 
 }
 
-module.exports = share;
-
-share.init();
+$(share.init);
