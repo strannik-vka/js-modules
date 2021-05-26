0ce9095e9194762e54377150fb88272a16971192
serj <zenitovic@gmail.com>
2021-05-26 16:32:54 +0300

    asd

--- file_change.js
+++ file_change.js
@@ -4,10 +4,11 @@
 */
 
 $(document)
-    .on('change', '[type="file"]', function () {
-        var names = [], name_elem = $('[file-change-name="' + $(this).attr('name') + '"]');
+    .on('change', '[type="file"]', function() {
+        var names = [],
+            name_elem = $('[file-change-name="' + $(this).attr('name') + '"]');
 
-        $.each($(this)[0].files, function (i, file) {
+        $.each($(this)[0].files, function(i, file) {
             names.push(file.name);
         });
 
@@ -18,12 +19,12 @@ $(document)
         name_elem.html('<span class="file-delete" delete></span>' + names.join(', '));
 
         if (names.length) {
-            $(this).parents('.input-group').addClass('active');
+            $(this).parents('.input-group, [file-group]').addClass('active');
         } else {
-            $(this).parents('.input-group').removeClass('active');
+            $(this).parents('.input-group, [file-group]').removeClass('active');
         }
     })
-    .on('click', '[file-change-name] [delete]', function (e) {
+    .on('click', '[file-change-name] [delete]', function(e) {
         e.preventDefault();
 
         var name_elem = $(this).parents('[file-change-name]'),
@@ -31,8 +32,10 @@ $(document)
 
         name_elem.html(name_elem.attr('text-original'));
 
-        input.parents('.input-group').removeClass('active');
+        input.parents('.input-group, [file-group]').removeClass('active');
         input.replaceWith(input.clone().val(''));
+
+        $('[name="' + name_elem.attr('file-change-name') + '"]').trigger('change');
 
         return false;
     });
--- label_up.js
+++ label_up.js
@@ -1,20 +1,32 @@
-$('input.form-control', 'input.form-type-text', 'textarea.form-type-text').each(function() {
+$('input.form-control', 'input.form-type-text, textarea.form-type-text').each(function() {
     if ($(this).prev().length) {
         if ($(this).prev()[0].tagName == 'LABEL' && $.trim($(this).val())) {
             $(this).prev().addClass('label-up');
         }
     }
 });
-$(document).on('focus', 'input.form-control, input.form-type-text, textarea.form-type-text', function() {
-    if ($(this).prev().length) {
-        if ($(this).prev()[0].tagName == 'LABEL') {
-            $(this).prev().addClass('label-up');
+
+$(document)
+    .on('change', 'input[type="file"]', function() {
+        if ($(this).prev().length) {
+            if ($(this).prev()[0].tagName == 'LABEL' && $.trim($(this).val())) {
+                $(this).prev().addClass('label-up');
+            } else {
+                $(this).prev().removeClass('label-up');
+            }
         }
-    }
-}).on('blur', 'input.form-control, input.form-type-text, textarea.form-type-text', function() {
-    if ($(this).prev().length) {
-        if ($(this).prev()[0].tagName == 'LABEL' && !$.trim($(this).val())) {
-            $(this).prev().removeClass('label-up');
+    })
+    .on('focus', 'input.form-control, input.form-type-text, textarea.form-type-text', function() {
+        if ($(this).prev().length) {
+            if ($(this).prev()[0].tagName == 'LABEL') {
+                $(this).prev().addClass('label-up');
+            }
         }
-    }
-});
+    })
+    .on('blur', 'input.form-control, input.form-type-text, textarea.form-type-text', function() {
+        if ($(this).prev().length) {
+            if ($(this).prev()[0].tagName == 'LABEL' && !$.trim($(this).val())) {
+                $(this).prev().removeClass('label-up');
+            }
+        }
+    });
