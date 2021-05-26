8f8179e732ba1aeb215da5446eaaca448a3d6cf2
strannik-vka <canbka13@gmail.com>
2021-05-26 11:35:10 +0300

    fix

--- validate.js
+++ validate.js
@@ -8,46 +8,132 @@ window.validate = {
 
     valid_text: {
         required: 'Заполните это поле',
-        max: 'Сократите текст',
-        min: 'Количество символов должно быть не менее :min',
+        max: {
+            numeric: 'Не может быть более :max.',
+            file: 'Размер файла превышает :max Килобайт(а).',
+            string: 'Сократите текст',
+            array: 'Количество элементов не может превышать :max.'
+        },
+        min: {
+            numeric: 'Должно быть не менее :min.',
+            file: 'Размер файла должен быть не менее :min Килобайт(а).',
+            string: 'Количество символов должно быть не менее :min',
+            array: 'Количество элементов должно быть не менее :min.',
+        },
         email: 'Введите корректный электронный адрес',
-        confirmed: 'Не совпадает с подтверждением'
+        confirmed: 'Не совпадает с подтверждением',
+        mimes: 'Неподдерживаемый тип файла'
     },
 
     valid: {
-        required: function (value) {
-            return $.trim(value);
+        required: function (input) {
+            return $.trim(validate.helper.value(input));
         },
-        email: function (value) {
+        email: function (input) {
             var pattern = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i;
-            return pattern.exec(value);
+            return pattern.exec(validate.helper.value(input));
         },
-        max: function (value, max) {
-            var length = validate.helper.length(value);
+        max: function (input, max) {
+            var length = validate.helper.length(input);
             return parseFloat(max) >= length;
         },
-        min: function (value, min) {
-            var length = validate.helper.length(value);
+        min: function (input, min) {
+            var length = validate.helper.length(input);
             return parseFloat(min) <= length;
         },
-        confirmed: function (value) {
-            var value2 = validate.helper.value(validate.name.replace('_confirmation', ''), validate.form);
-            return value == value2;
+        confirmed: function (input) {
+            var val1 = validate.helper.value(input),
+                val2 = validate.helper.value(validate.name.replace('_confirmation', ''));
+
+            return val1 == val2;
+        },
+        mimes: function (input, mimes) {
+            var types = validate.helper.file.types(input),
+                result = false;
+
+            $.each(types, function (index, type) {
+                if (mimes.indexOf(type) == -1) {
+                    result = type;
+                    return false;
+                }
+            });
+
+            return result == false;
+        },
+        getText: function (method, args, text, type, name) {
+            var valid_text = validate.valid_text[method];
+
+            if (typeof valid_text === 'object') {
+                valid_text = valid_text[type];
+            }
+
+            if (typeof text === 'object' && text != null) {
+                if (text[name + '.' + method]) {
+                    valid_text = text[name + '.' + method];
+                }
+            }
+
+            return valid_text.replace(':' + method, args[0]);
         }
     },
 
     helper: {
-        length: function (value) {
-            return $.trim(value).replace(/\n/g, '\n\r').length;
-        },
-        value: function (name, form) {
-            var elem = form.find('[name="' + name + '"]');
+        type: function (input) {
+            var type = 'string',
+                elem_type = input.attr('data-type') ? input.attr('data-type') : input.attr('type');
 
-            if (elem.attr('type') == 'radio') {
-                elem = form.find('[name="' + name + '"]:checked');
+            if (elem_type == 'file') {
+                type = 'file';
             }
 
-            return $.trim(elem.val());
+            if (elem_type == 'number') {
+                type = 'numeric';
+            }
+
+            if (elem_type == 'array') {
+                type = 'array';
+            }
+
+            return type;
+        },
+        file: {
+            size: function (input) {
+                var bytes = 0;
+
+                $.each(input[0].files, function (index, file) {
+                    bytes += file.size;
+                });
+
+                return parseFloat((bytes / 1024).toFixed(3));
+            },
+            types: function (input) {
+                var types = [];
+
+                $.each(input[0].files, function (index, file) {
+                    var arr = file.name.split('.');
+                    types.push(arr[arr.length - 1]);
+                });
+
+                return types;
+            }
+        },
+        length: function (input) {
+            var length = false;
+
+            if (input.attr('type') == 'file') {
+                length = validate.helper.file.size(input);
+            } else {
+                length = $.trim(input.val()).replace(/\n/g, '\n\r').length
+            }
+
+            return length;
+        },
+        value: function (input) {
+            if (input.attr('type') == 'radio') {
+                input = validate.form.find('[name="' + input.attr('name') + '"]:checked');
+            }
+
+            return $.trim(input.val());
         }
     },
 
@@ -62,36 +148,30 @@ window.validate = {
 
         $.each(obj, function (name, methods) {
             validate.form = form;
+            validate.name = name;
 
-            var value = validate.helper.value(name, form),
+            var input = form.find('[name="' + name + '"]'),
+                type = validate.helper.type(input),
                 methods = methods.split('|');
 
             $.each(methods, function (i, method) {
-                validate.name = name;
-
                 var args = method.split(':'),
                     method = args.splice(0, 1);
 
                 var result = args.length
-                    ? validate.valid[method](value, args[0])
-                    : validate.valid[method](value);
+                    ? validate.valid[method](input, args[0])
+                    : validate.valid[method](input);
 
                 if (!result) {
                     if (!errors[name]) {
                         errors[name] = [];
                     }
 
-                    var valid_text = validate.valid_text[method];
+                    var valid_text = validate.valid.getText(method, args, text, type, name);
 
-                    if (typeof text === 'object' && text != null) {
-                        if (text[name + '.' + method]) {
-                            valid_text = text[name + '.' + method];
-                        }
+                    if (valid_text) {
+                        errors[name].push(valid_text);
                     }
-
-                    valid_text = valid_text.replace(':' + method, args[0]);
-
-                    errors[name].push(valid_text);
                 }
             });
         });
