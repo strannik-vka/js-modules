3a92148bd0f8cfd5b1588fe4c1678b699c6a7e48
strannik-vka <canbka13@gmail.com>
2021-05-27 09:20:11 +0300

    fix

--- label_up.js
+++ label_up.js
@@ -1,4 +1,4 @@
-$('input.form-control', 'input.form-type-text, textarea.form-type-text').each(function() {
+$('input.form-control, input.form-type-text, textarea.form-type-text').each(function() {
     if ($(this).prev().length) {
         if ($(this).prev()[0].tagName == 'LABEL' && $.trim($(this).val())) {
             $(this).prev().addClass('label-up');
