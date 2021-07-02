9468f6aefb6e73df001eb4fed2bb23ff3741b648
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2021-07-02 13:57:00 +0300

    Create count.js

---
+++ count.js
@@ -0,0 +1,24 @@
+var count = $('[count-numbers]');
+
+$(function () {
+    count.each(function () {
+        $(this).prop('Counter', 0)
+            .css('opacity', '1')
+            .animate({
+                Counter: $(this).text()
+            }, {
+                duration: 3500,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text(commaSeparateNumber(Math.ceil(now)));
+                }
+            });
+    });
+
+    function commaSeparateNumber(val) {
+        while (/(\d+)(\d{3})/.test(val.toString())) {
+            val = val.toString().replace(/(\d+)(\d{3})/, '$1' + '.' + '$2');
+        }
+        return val;
+    }
+});
