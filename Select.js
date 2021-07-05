803eafea66fcd707448daf9a18ce9e2145f7be86
strannik-vka <canbka13@gmail.com>
2021-07-05 23:58:32 +0300

    опа

--- Select.js
+++ Select.js
@@ -1,28 +1,103 @@
 class Select {
-    constructor(selector) {
-        this.selector = selector;
+    constructor() {
+        this.selector = {
+            parent: '[data-select]',
+            title: '[data-select-title]',
+            options: '[data-select-options]',
+            option: '[data-select-option]',
+            type: '[data-select-type]',
+            query: '[data-select-query]'
+        };
+
+        this.ajaxTimer = false;
 
         this.events();
-        this.each(selector);
+        this.each();
     }
 
-    each(selector) {
-        $(this.selector.parent).each(function () {
-            $(this).find(selector.title).attr('data-select-title', $(this).find(selector.title).text());
+    select(select) {
+        var options = select.find(this.selector.options),
+            title = select.find(this.selector.title);
 
-            if ($(this).find('input:checked').length) {
-                $(this).trigger('change');
+        return {
+            title: title,
+            options: options,
+            type: options.attr('data-select-type'),
+            selected: select.find('input:checked').length,
+            query: select.find(this.selector.query).length
+                ? $.trim(select.find(this.selector.query).val())
+                : null,
+            url: select.attr('data-select')
+                ? select.attr('data-select')
+                : null,
+            name: options.attr('data-select-options')
+                ? options.attr('data-select-options')
+                : select.find('[name]:eq(-1)').attr('name'),
+            placeholder: title.attr('data-select-title')
+                ? title.attr('data-select-title')
+                : $.trim(title.text())
+        };
+    }
+
+    each() {
+        $(this.selector.parent).each((i, item) => {
+            var select = this.select($(item));
+
+            select.title.attr('data-select-title', select.placeholder);
+
+            if (select.selected) {
+                $(item).trigger('change');
+            }
+
+            if (select.url) {
+                this.ajax($(item));
             }
         });
     }
 
+    ajax(select) {
+        var select = this.select(select);
+
+        $.get(select.url, {
+            name: select.query
+        }, (json) => {
+            select.options.find(this.selector.option).each((i, item) => {
+                if (!$(item).find('input').is(':checked')) {
+                    $(item).remove();
+                }
+            });
+
+            $.each(json.data, (i, item) => {
+                select.options.append('\
+                    <div class="__select__checkbox" data-select-option>\
+                        <input name="'+ select.name + '" id="' + select.name + '_' + i + '" class="__select__input" type="' + select.type + '" value="' + item.id + '" />\
+                        <label for="' + select.name + '_' + i + '" class="__select__label">' + item.name + '</label>\
+                    </div>\
+                ');
+            });
+        });
+    }
+
+    search(e) {
+        var select = $(e.currentTarget).parents(this.selector.parent);
+
+        if (this.ajaxTimer) clearTimeout(this.ajaxTimer);
+
+        this.ajaxTimer = setTimeout(() => {
+            this.ajax(select);
+        }, 1000);
+    }
+
     events() {
         $(document)
+            .on('input', this.selector.query, (e) => {
+                this.search(e);
+            })
             .on('change', this.selector.parent + ' input', (e) => {
                 this.change(e);
             })
-            .on('click', this.selector.title, (e) => {
-                this.titleClick(e);
+            .on('click', this.selector.title + ', ' + this.selector.query, (e) => {
+                this.toggleActive(e);
             })
             .on('click', (e) => {
                 this.closest(e);
@@ -35,10 +110,10 @@ class Select {
         }
     }
 
-    titleClick(e) {
+    toggleActive(e) {
         var parent = $(e.currentTarget).parents(this.selector.parent);
 
-        if (parent.hasClass('active')) {
+        if (parent.hasClass('active') && $(e.currentTarget)[0].tagName != 'INPUT') {
             parent.removeClass('active');
         } else {
             parent.addClass('active');
@@ -65,7 +140,4 @@ class Select {
     }
 }
 
-export default new Select({
-    parent: '[data-select]',
-    title: '[data-select-title]'
-});
+export default new Select();
