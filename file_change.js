450f35e45a02c286da232b0f8a077f344c842275
strannik-vka <canbka13@gmail.com>
2021-07-07 09:25:02 +0300

    fix

--- file_change.js
+++ file_change.js
@@ -4,11 +4,14 @@
 */
 
 $(document)
-    .on('change', '[type="file"]', function() {
+    .on('change', '[type="file"]', function () {
         var names = [],
-            name_elem = $('[file-change-name="' + $(this).attr('name') + '"]');
+            parent = $(this).parents('.input-group, [file-group]'),
+            name_elem = parent.length
+                ? parent.find('[file-change-name="' + $(this).attr('name') + '"]')
+                : $('[file-change-name="' + $(this).attr('name') + '"]');
 
-        $.each($(this)[0].files, function(i, file) {
+        $.each($(this)[0].files, function (i, file) {
             names.push(file.name);
         });
 
@@ -19,23 +22,25 @@ $(document)
         name_elem.html('<span class="file-delete" delete></span>' + names.join(', '));
 
         if (names.length) {
-            $(this).parents('.input-group, [file-group]').addClass('active');
+            parent.addClass('active');
         } else {
-            $(this).parents('.input-group, [file-group]').removeClass('active');
+            parent.removeClass('active');
         }
     })
-    .on('click', '[file-change-name] [delete]', function(e) {
+    .on('click', '[file-change-name] [delete]', function (e) {
         e.preventDefault();
 
-        var name_elem = $(this).parents('[file-change-name]'),
-            input = $('[name="' + name_elem.attr('file-change-name') + '"]');
+        var parent = input.parents('.input-group, [file-group]'),
+            name_elem = $(this).parents('[file-change-name]'),
+            input = parent.length
+                ? parent.find('[name="' + name_elem.attr('file-change-name') + '"]')
+                : $('[name="' + name_elem.attr('file-change-name') + '"]');
 
         name_elem.html(name_elem.attr('text-original'));
 
-        input.parents('.input-group, [file-group]').removeClass('active');
+        parent.removeClass('active');
         input.replaceWith(input.clone().val(''));
-
-        $('[name="' + name_elem.attr('file-change-name') + '"]').trigger('change');
+        input.trigger('change');
 
         return false;
     });
