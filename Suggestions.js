261be9c6e3cb2eb46ac3935d9fd3f92e92253ed8
strannik-vka <canbka13@gmail.com>
2021-07-08 01:04:55 +0300

    Города и регионы

---
+++ Suggestions.js
@@ -0,0 +1,80 @@
+require('./jquery.suggestions');
+
+export default class Suggestions {
+
+    constructor(obj) {
+        this.token = obj.token;
+    }
+
+    uniqValues(json, field) {
+        var values = [],
+            result = [];
+
+        $.each(json, function (i, item) {
+            if (item.data[field] && values.indexOf(item.data[field]) == -1) {
+                values.push(item.data[field]);
+                result.push(item);
+            }
+        });
+
+        return result;
+    }
+
+    country(elem) {
+        if (elem.length) {
+            elem.suggestions({
+                token: this.token,
+                type: "country",
+                hint: false,
+            });
+        }
+    }
+
+    region(elem) {
+        if (elem.length) {
+            elem.suggestions({
+                token: this.token,
+                type: "address",
+                hint: false,
+                count: 1,
+                bounds: "region",
+                formatSelected: function (suggestion) {
+                    return suggestion.data.region_with_type;
+                },
+                formatResult: function (value, currentValue, suggestion, options) {
+                    suggestion.value = suggestion.data.region_with_type;
+                    return $.Suggestions.prototype.formatResult.call(this, suggestion.value, currentValue, suggestion, options);
+                },
+                onSuggestionsFetch: (json) => {
+                    return this.uniqValues(json, 'region_with_type');
+                }
+            });
+        }
+    }
+
+    city(elem) {
+        if (elem.length) {
+            elem.suggestions({
+                token: this.token,
+                type: "address",
+                hint: false,
+                bounds: "city",
+                constraints: {
+                    locations: { city_type_full: "город" }
+                },
+                formatSelected: function (suggestion) {
+                    return suggestion.data.city;
+                },
+                formatResult: function (value, currentValue, suggestion, options) {
+                    var newValue = suggestion.data.city;
+                    suggestion.value = newValue;
+                    return $.Suggestions.prototype.formatResult.call(this, newValue, currentValue, suggestion, options);
+                },
+                onSuggestionsFetch: (json) => {
+                    return this.uniqValues(json, 'city');
+                }
+            });
+        }
+    }
+
+}
