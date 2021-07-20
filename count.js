4b3518483b640fafb4f19239c946478c695b78cb
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2021-07-20 12:32:12 +0300

    Update count.js

--- count.js
+++ count.js
@@ -17,7 +17,7 @@ $(function () {
 
     function commaSeparateNumber(val) {
         while (/(\d+)(\d{3})/.test(val.toString())) {
-            val = val.toString().replace(/(\d+)(\d{3})/, '$1' + '.' + '$2');
+            val = val.toString().replace(/(\d+)(\d{3})/, '$1' + ' ' + '$2');
         }
         return val;
     }
