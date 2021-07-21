9f6616dd7cdee9ca625710c7cd455ad0d19e254a
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2021-07-21 11:05:56 +0300

    Update custom_checkbox.js

--- custom_checkbox.js
+++ custom_checkbox.js
@@ -2,9 +2,8 @@ custom_checkbox = {
     init: function () {
         var customCheckbox = $('[custom-checkbox]');
 
-        customCheckbox.on('click', function () {
-            $(this).find('[custom-checkbox-input]').trigger('click');
-            return false;
+        $(document).each('click', 'customCheckbox', function () {
+            $(this).find('[custom-checkbox-input]').prop('checked', true);
         });
     }
 }
