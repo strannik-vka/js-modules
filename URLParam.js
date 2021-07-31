f61630ca36c907f3a5f7b9869057c3fad51148bc
strannik-vka <canbka13@gmail.com>
2021-07-31 13:42:29 +0300

    fix

--- URLParam.js
+++ URLParam.js
@@ -1,7 +1,9 @@
-window.$.url_get = function (name) {
+window.URLParam = (name) => {
     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+
     if (results == null) {
         return null;
     }
+
     return decodeURI(results[1]) || 0;
 }
