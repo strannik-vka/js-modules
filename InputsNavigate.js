a070d107d388631e1ec8c53fa306a68aa85eecc6
strannik-vka <canbka13@gmail.com>
2021-08-02 01:57:01 +0300

    fix

--- InputsNavigate.js
+++ InputsNavigate.js
@@ -2,13 +2,9 @@ class InputsNavigate {
 
     constructor() {
         $(document)
-            .on('click', '[data-inputs-prev]', (e) => {
+            .on('click', '[data-inputs-prev], [data-inputs-next]', (e) => {
                 this.currentElems(e);
-                this.prev();
-            })
-            .on('click', '[data-inputs-next]', (e) => {
-                this.currentElems(e);
-                this.next();
+                this.buttonClick();
             })
             .on('input', '[data-inputs-navigate] [data-inputs-input]:eq(-1) input', (e) => {
                 this.currentElems(e);
@@ -16,56 +12,68 @@ class InputsNavigate {
             });
     }
 
+    currentElems(e) {
+        this.button = $(e.currentTarget);
+        this.parent = this.button.parents('[data-inputs-navigate]');
+        this.currentInput = this.parent.find('[data-inputs-input].active');
+        this.isNext = this.button.attr('data-inputs-next') != undefined;
+        this.prevInput = this.currentInput.prev('[data-inputs-input]');
+        this.nextInput = this.currentInput.next('[data-inputs-input]');
+        this.showInput = this.isNext ? this.nextInput : this.prevInput;
+        this.showInputNext = this.isNext ?
+            this.showInput.next('[data-inputs-input]') :
+            this.showInput.prev('[data-inputs-input]');
+    }
+
+    isErrors() {
+        return this.parent.find('[data-error-input]').length;
+    }
+
+    showErrorInput() {
+        $('[data-inputs-input]').each((i, item) => {
+            if ($(item).find('[data-error-input]').length) {
+                this.parent = $(item).parents('[data-inputs-navigate]');
+                this.currentInput = this.parent.find('[data-inputs-input].active');
+                this.showInput = $(item);
+
+                this.show();
+
+                return false;
+            }
+        });
+
+        $('[data-inputs-prev], [data-inputs-next]').removeClass('active').addClass('deactive');
+    }
+
     changeEndInput(e) {
         var val = $.trim($(e.currentTarget).val());
 
-        if (val) {
+        if (val && !this.isErrors()) {
             $('[data-inputs-end-show]').show();
         } else {
             $('[data-inputs-end-show]').hide();
         }
     }
 
-    currentElems(e) {
-        this.button = $(e.currentTarget);
-        this.parent = $(e.currentTarget).parents('[data-inputs-navigate]');
-        this.currentInput = this.parent.find('[data-inputs-input].active');
-    }
+    show() {
+        if (this.showInput.length) {
+            this.currentInput.hide().removeClass('active');
+            this.showInput.addClass('active').show();
 
-    prev() {
-        if (!this.currentInput.hasClass('deactive')) {
-            if (this.currentInput.prev('[data-inputs-input]').length) {
-                this.currentInput.fadeOut(400, () => {
-                    this.currentInput.removeClass('active');
-                    this.currentInput.prev().addClass('active').fadeIn(500);
+            this.parent.find('.deactive').removeClass('deactive');
 
-                    this.parent.find('.deactive').removeClass('deactive');
-
-                    if (this.currentInput.prev().prev('[data-inputs-input]').length == 0) {
-                        this.button.addClass('deactive');
-                    }
-                });
+            if (this.showInputNext.length == 0) {
+                this.button.addClass('deactive');
             }
         }
     }
 
-    next() {
-        if (!this.currentInput.hasClass('deactive')) {
-            if (this.currentInput.next('[data-inputs-input]').length) {
-                this.currentInput.fadeOut(400, () => {
-                    this.currentInput.removeClass('active');
-                    this.currentInput.next().addClass('active').fadeIn(500);
-
-                    this.parent.find('.deactive').removeClass('deactive');
-
-                    if (this.currentInput.next().next('[data-inputs-input]').length == 0) {
-                        this.button.addClass('deactive');
-                    }
-                });
-            }
+    buttonClick() {
+        if (!this.button.hasClass('deactive')) {
+            this.show();
         }
     }
 
 }
 
-new InputsNavigate();
+export default new InputsNavigate();
