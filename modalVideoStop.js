1534461276aafd6ab456024a01a595a525dccb6c
strannik-vka <canbka13@gmail.com>
2021-08-02 12:14:22 +0300

    fix

--- modalVideoStop.js
+++ modalVideoStop.js
@@ -2,4 +2,8 @@ $(document).on('hidden.bs.modal', '.modal', function () {
     $(this).find('iframe').each(function () {
         $(this).after($(this).clone()).remove();
     });
+
+    $(this).find('video').each(function () {
+        $(this).after($(this).clone()).remove();
+    });
 });
