8992f6e5dcff2d5ab42ecec8e1ca4e0f827c5537
strannik-vka <canbka13@gmail.com>
2022-01-13 12:15:27 +0300

    fix

--- Debug.js
+++ Debug.js
@@ -27,24 +27,26 @@ class Debug {
 
     onerror() {
         window.onerror = (msg, url, lineNo, columnNo, error) => {
-            var text = '';
+            if (lineNo && columnNo) {
+                var text = '';
 
-            text += navigator.userAgent + "\n";
-            text += 'url: ' + url + "\n";
-            text += 'msg: ' + msg + "\n";
-            text += 'lineNo: ' + lineNo + "\n";
-            text += 'columnNo: ' + columnNo + "\n";
-            text += 'page: ' + location.href + "\n";
+                text += navigator.userAgent + "\n";
+                text += 'url: ' + url + "\n";
+                text += 'msg: ' + msg + "\n";
+                text += 'lineNo: ' + lineNo + "\n";
+                text += 'columnNo: ' + columnNo + "\n";
+                text += 'page: ' + location.href + "\n";
 
-            if(typeof error === 'object' && error != null) {
-                text += 'error: ' + JSON.stringify(error.stack ? error.stack : error) + "\n";
+                if (typeof error === 'object' && error != null) {
+                    text += 'error: ' + JSON.stringify(error.stack ? error.stack : error) + "\n";
+                }
+
+                if (typeof user === 'object' && user != null) {
+                    text += 'user: ' + JSON.stringify(user) + "\n";
+                }
+
+                this.send(text);
             }
-
-            if(typeof user === 'object' && user != null) {
-                text += 'user: ' + JSON.stringify(user) + "\n";
-            }
-
-            this.send(text);
         }
     }
 
