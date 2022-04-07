a56c4e481f7124dc14950cfcdf92e3a16908a2ea
strannik-vka <canbka13@gmail.com>
2022-04-07 22:41:06 +0300

    fix

--- check-list.js
+++ check-list.js
@@ -1,13 +1,10 @@
 $(document).on('change', '[data-check-list] [name]', (e) => {
-    let val = $(e.currentTarget).val(),
+    let val = null,
         id = $(e.currentTarget).parents('[data-check-list]').attr('data-check-list');
 
-
-    if ($(e.currentTarget).attr('value') == undefined) {
-        let nextElem = $(e.currentTarget).next();
-        if (nextElem[0].tagName == 'LABEL') {
-            val = $.trim(nextElem.text());
-        }
+    let nextElem = $(e.currentTarget).next();
+    if (nextElem[0].tagName == 'LABEL') {
+        val = $.trim(nextElem.text());
     }
 
     $('[data-check-value="' + id + '"]').val(val).trigger('change');
