d438ce9213860183affaa6be36eb3d9aa4bab712
strannik-vka <canbka13@gmail.com>
2022-04-12 20:18:38 +0300

    disabled

--- check-list.js
+++ check-list.js
@@ -1,11 +1,18 @@
-$(document).on('change', '[data-check-list] [name]', (e) => {
-    let val = null,
-        id = $(e.currentTarget).parents('[data-check-list]').attr('data-check-list');
+$(document)
+    .on('mousedown', '[data-bs-toggle="dropdown"]', (e) => {
+        if ($(e.currentTarget).find('[disabled]').length) {
+            $(e.currentTarget).dropdown('toggle');
+            $('[aria-labelledby="' + $(e.currentTarget).attr('id') + '"]').hide();
+        }
+    })
+    .on('change', '[data-check-list] [name]', (e) => {
+        let val = null,
+            id = $(e.currentTarget).parents('[data-check-list]').attr('data-check-list');
 
-    let nextElem = $(e.currentTarget).next();
-    if (nextElem[0].tagName == 'LABEL') {
-        val = $.trim(nextElem.text());
-    }
+        let nextElem = $(e.currentTarget).next();
+        if (nextElem[0].tagName == 'LABEL') {
+            val = $.trim(nextElem.text());
+        }
 
-    $('[data-check-value="' + id + '"]').val(val).trigger('change');
-});
+        $('[data-check-value="' + id + '"]').val(val).trigger('change');
+    });
