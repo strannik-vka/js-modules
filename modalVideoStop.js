dc04ab9241542a1161e44dddca209d8e8d70a47c
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2022-06-16 19:14:56 +0300

    Update modalVideoStop.js

--- modalVideoStop.js
+++ modalVideoStop.js
@@ -1,12 +1,20 @@
+const videoReplace = (video) => {
+    let clone = video.clone();
+
+    if (clone.hasClass('lazyloaded')) {
+        clone.removeClass('lazyloaded');
+        clone.attr('src', clone.attr('data-src'));
+    }
+
+    video.after(clone).remove();
+}
+
 $(document).on('hidden.bs.modal', '.modal', function () {
-    $(this).find('iframe video').each(function () {
-        let clone = $(this).clone();
+    $(this).find('iframe').each(function () {
+        videoReplace($(this));
+    });
 
-        if (clone.hasClass('lazyloaded')) {
-            clone.removeClass('lazyloaded');
-            clone.attr('src', clone.attr('data-src'));
-        }
-
-        $(this).after(clone).remove();
+    $(this).find('video').each(function () {
+        videoReplace($(this));
     });
 });
