006bc0378172048cf9b90c3a685fb6d58aa8d5fd
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2022-06-16 19:08:08 +0300

    Update modalVideoStop.js

--- modalVideoStop.js
+++ modalVideoStop.js
@@ -1,9 +1,9 @@
 $(document).on('hidden.bs.modal', '.modal', function () {
     $(this).find('iframe').each(function () {
-        $(this).after($(this).clone()).remove();
+        $(this).after($(this).removeClass('lazyloaded').clone()).remove();
     });
 
     $(this).find('video').each(function () {
-        $(this).after($(this).clone()).remove();
+        $(this).after($(this).removeClass('lazyloaded').clone()).remove();
     });
 });
