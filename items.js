6cccc48ee8b3e42887a60de5f32ef3cac4a5c800
strannik-vka <canbka13@gmail.com>
2022-07-20 09:42:27 +0300

    ajax=true

--- items.js
+++ items.js
@@ -471,6 +471,12 @@ window.items = {
                 data.page = options.page;
             }
 
+            if (model.url.indexOf('?') > -1) {
+                model.url += '&ajax=true';
+            } else {
+                model.url += '?ajax=true';
+            }
+
             ajax({
                 url: model.url,
                 data: data,
