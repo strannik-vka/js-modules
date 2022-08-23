12fc8de4926cefd4ed1dee6e9f86ef8a688a7273
strannik-vka <canbka13@gmail.com>
2022-08-23 10:33:33 +0300

    form progress

---
+++ FormProgress.js
@@ -0,0 +1,37 @@
+const getValue = (input, form) => {
+    if (input.attr('type') == 'radio') {
+        input = form.find('[name="' + input.attr('name') + '"]:checked');
+    }
+
+    return $.trim(input.val());
+}
+
+export default (form) => {
+    let all = 0, success = 0, radios = [];
+
+    form.find('[name]').each((i, item) => {
+        let input = $(item),
+            name = input.attr('name'),
+            val = getValue(input, form);
+
+        if (input.attr('type') == 'hidden' || input.attr('readonly') || input.attr('disabled')) {
+            return true;
+        }
+
+        if (input.attr('type') == 'radio') {
+            if (radios.indexOf(name) > -1) {
+                return true;
+            }
+
+            radios.push(name);
+        }
+
+        if (val) {
+            success++;
+        }
+
+        all++;
+    });
+
+    return success && all ? Math.ceil(100 / (all / success)) : 0;
+}
