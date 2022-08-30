214a322b3a1ccb67006b09dd66673b1cddaf2d09
strannik-vka <canbka13@gmail.com>
2022-08-30 10:14:23 +0300

    fix

--- otherVariant.js
+++ otherVariant.js
@@ -1,4 +1,14 @@
 $(document)
+    .on('change', '[type="radio"]', (e) => {
+        if (!$(e.currentTarget).attr('data-ov-input')) {
+            let inputElem = $('[name="' + $(e.currentTarget).attr('name') + '"][data-ov-input]');
+
+            if (inputElem.length) {
+                let textElem = $('[data-ov-text="' + inputElem.attr('data-ov-input') + '"]');
+                textElem.val('');
+            }
+        }
+    })
     .on('click', '[data-ov-input]', (e) => {
         let inputElem = $(e.currentTarget),
             textElem = $('[data-ov-text="' + inputElem.attr('data-ov-input') + '"]');
@@ -23,5 +33,9 @@ $(document)
         $('[data-ov-input="' + $(e.currentTarget).attr('data-ov-text') + '"]').prop('checked', true);
     })
     .on('input', '[data-ov-text]', (e) => {
-        $('[data-ov-input="' + $(e.currentTarget).attr('data-ov-text') + '"]').val('other:' + $(e.currentTarget).val());
+        let inputElem = $('[data-ov-input="' + $(e.currentTarget).attr('data-ov-text') + '"]');
+
+        if (inputElem.attr('type') == 'radio') {
+            $('[data-ov-input="' + $(e.currentTarget).attr('data-ov-text') + '"]').val('other:' + $(e.currentTarget).val());
+        }
     });
