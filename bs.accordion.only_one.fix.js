dc10f46e8cabb060d9b2064c0a3954b70aa393ac
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2022-09-01 15:51:19 +0300

    Create bs.accordion.only_one.fix.js

---
+++ bs.accordion.only_one.fix.js
@@ -0,0 +1,16 @@
+// Чтобы открывался один collapse в .accordion-item
+
+const selector = '[data-only-one]';
+
+$(document)
+    .on('shown.bs.collapse', selector + ' .collapse', (e) => {
+        $(selector + ' [data-ac-toggle]').removeAttr('data-ac-toggle').attr('data-bs-toggle', 'collapse');
+    })
+    .on('click', selector + ' [data-bs-toggle="collapse"]', (e) => {
+        $(selector + ' [data-bs-toggle]').removeAttr('data-bs-toggle').attr('data-ac-toggle', 'true');
+    })
+    .on('click', selector + ' [data-ac-toggle]', (e) => {
+        if ($(selector + ' .collapse.show').length == 0 && $(selector + ' .collapsing').length == 0) {
+            $($(e.currentTarget).attr('data-bs-target')).collapse('toggle');
+        }
+    });
