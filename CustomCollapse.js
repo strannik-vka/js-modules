dc535178293a322137f970164e56326a23c997a6
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2022-11-01 23:11:20 +0300

    Update CustomCollapse.js

--- CustomCollapse.js
+++ CustomCollapse.js
@@ -69,7 +69,7 @@ class CustomCollapse {
                     height = parseFloat($(item).css('height'));
                     $(item).removeAttr('style');
                     this.elemAnimate(item, height);
-                }, 10);
+                }, 100);
             } else {
                 height = parseFloat($(item).css('height'));
                 $(item).removeAttr('style');
