2eeccbd4c7fba7063bdf2a7a51a83a9c06e98451
strannik-vka <canbka13@gmail.com>
2022-11-03 15:10:47 +0300

    selection

---
+++ Selection.js
@@ -0,0 +1,144 @@
+/*
+    Атрибуты:
+    data-selection | родитель, элемент select
+*/
+
+class Selection {
+
+    constructor(options) {
+        options = typeof options === 'object' && options != null ? options : {};
+
+        this.selector = options.selector ? options.selector : '[data-selection]';
+        this.placeholder = options.placeholder ? options.placeholder : '';
+
+        this.init();
+        this.events();
+    }
+
+    init() {
+        $(this.selector).each((i, select) => {
+            $(select)
+                .hide()
+                .after(this.getTemplate($(select)));
+        });
+    }
+
+    events() {
+        $(document)
+            .on('click', '.selection__select', (e) => {
+                this.selectOnClick($(e.currentTarget));
+            })
+            .on('click', '.selection__option', (e) => {
+                this.optionOnClick($(e.currentTarget));
+            })
+            .on('mouseup', (e) => {
+                if ($('.selection').has(e.target).length === 0) {
+                    this.hideAll();
+                }
+            });
+    }
+
+    selectOnClick(selection_select) {
+        let selection = selection_select.parents('.selection'),
+            isShow = selection.hasClass('active');
+
+        if (isShow) {
+            this.hide(selection);
+        } else {
+            this.show(selection);
+        }
+    }
+
+    optionOnClick(selection_option) {
+        let selection = selection_option.parents('.selection'),
+            isActive = selection_option.hasClass('active'),
+            value = selection_option.attr('data-value'),
+            isMultiple = selection.hasClass('multiple'),
+            select = selection.prev();
+
+        if (isActive) {
+            selection_option.removeClass('active');
+            select.find('[value="' + value + '"]').prop('selected', false);
+        } else {
+            selection_option.addClass('active');
+            select.find('[value="' + value + '"]').prop('selected', true);
+        }
+
+        let count = selection.find('.active').length;
+
+        selection.find('.selection__count').html(count);
+
+        if (count) {
+            selection.addClass('selected');
+        } else {
+            selection.removeClass('selected');
+        }
+
+        let values = [];
+
+        selection.find('.active').each((i, option) => {
+            values.push($(option).html());
+        });
+
+        if (values.length == 0) {
+            values.push(select.attr('placeholder') ? select.attr('placeholder') : this.placeholder);
+        }
+
+        selection.find('.selection__placeholder').html(values.join(', '));
+
+        if (!isMultiple) {
+            this.hide(selection);
+        }
+
+        select.trigger('change');
+    }
+
+    show(selection) {
+        selection.addClass('active');
+    }
+
+    hide(selection) {
+        selection.removeClass('active');
+    }
+
+    hideAll() {
+        $('.selection.active').removeClass('active');
+    }
+
+    getTemplate(select) {
+        let selection = '', selection_select = '', selection_options = '',
+            placeholder = this.placeholder,
+            isMultiple = select.attr('multiple');
+
+        if (select.attr('placeholder')) {
+            placeholder = select.attr('placeholder');
+        }
+
+        if (placeholder) {
+            selection_select += '<div class="selection__placeholder">' + placeholder + '</div>';
+        }
+
+        if (isMultiple) {
+            selection_select += '<div class="selection__count"></div>';
+        }
+
+        selection_select += '<div class="selection__toggle"></div>';
+
+        selection += '<div class="selection__select">' + selection_select + '</div>';
+
+        select.find('option').each((i, item) => {
+            selection_options += '<div data-value="' + $(item).val() + '" class="selection__option">' + $(item).text() + '</div>';
+        });
+
+        selection += '<div class="selection__options">' + selection_options + '</div>';
+
+        return '<div class="selection ' + (isMultiple ? 'multiple' : '') + '">' + selection + '</div>';
+    }
+
+}
+
+$(() => {
+    new Selection();
+})
+
+export default Selection;
