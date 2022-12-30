003b32ce150be44b0a7795b8c9433cf0c28a425b
strannik-vka <canbka13@gmail.com>
2022-12-30 12:36:39 +0300

    fix

--- MixColors.js
+++ MixColors.js
@@ -1,6 +1,22 @@
 class MixColors {
 
 	get(rgb1, rgb2, percent1, percent2) {
+		if (percent1 > 100) {
+			percent1 = 100;
+		}
+
+		if (percent1 < 0) {
+			percent1 = 0;
+		}
+
+		if (percent2 > 100) {
+			percent2 = 100;
+		}
+
+		if (percent2 < 0) {
+			percent2 = 0;
+		}
+
 		let red = this.mix(rgb1[0], rgb2[0], percent1, percent2),
 			green = this.mix(rgb1[1], rgb2[1], percent1, percent2),
 			blue = this.mix(rgb1[2], rgb2[2], percent1, percent2);
