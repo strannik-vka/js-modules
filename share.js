73e431a1344a3afa78aac233d0cfe8e700a77c11
strannik-vka <canbka13@gmail.com>
2023-07-11 10:25:00 +0300

    ok

--- share.js
+++ share.js
@@ -5,7 +5,8 @@ var share = {
         twitter: 'https://twitter.com/share?url=URL&text=TITLE',
         linkedin: 'https://www.linkedin.com/sharing/share-offsite/?url=URL',
         vk: 'https://vk.com/share.php?url=URL&title=TITLE',
-        tg: 'https://telegram.me/share/url?url=URL&text=TITLE'
+        tg: 'https://telegram.me/share/url?url=URL&text=TITLE',
+        ok: 'https://connect.ok.ru/dk?st.cmd=WidgetSharePreview&st.shareUrl=URL'
     },
 
     social: function (name, url, title) {
